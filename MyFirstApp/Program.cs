b533a38f37ac4057c34dc1439f985379bebe3e49
Matias H. Nurmik <matiasnurmik@hotmail.com>
2024-09-22 15:06:43 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //output
